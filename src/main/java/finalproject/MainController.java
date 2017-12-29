274181f9583e2a94d597c959b11e824ce88c87f4
Jeong Eui Jin <jej8076@gmail.com>
2017-12-29 17:16:08 +0900

    test

--- src/main/java/finalproject/MainController.java
+++ src/main/java/finalproject/MainController.java
@@ -1,5 +1,20 @@
-package finalproject;
+package part01;
 
-public class MainController {
-	 
-}
+import org.mybatis.spring.SqlSessionTemplate;
+
+public class MemDaoImp implements MemDAO {
+   private SqlSessionTemplate sqlSession;
+
+   public MemDaoImp() {
+
+   }
+
+   public void setSqlSession(SqlSessionTemplate sqlSession) {
+      this.sqlSession = sqlSession;
+   }
+
+   @Override
+   public void insertMethod(MemDTO dto) {
+      sqlSession.insert("mem.ins", dto);
+   }
+} // end class
