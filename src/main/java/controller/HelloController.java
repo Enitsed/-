e9c56c36ccddc3a75092233b5a3afc8d35f2d2d2
gkdl33 <limyoung92@naver.com>
2018-01-18 17:12:20 +0900

    dto

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -3,12 +3,9 @@ package controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-<<<<<<< HEAD
 // http://192.168.10.240:8090/finalproject/main
-=======
 // http://localhost:8090/finalproject/main
 
->>>>>>> b6b726e261e53331cd617b72f27c2babb7723141
 @Controller
 public class HelloController {
 	
