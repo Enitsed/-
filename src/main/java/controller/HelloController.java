7ff76ab62e590716d1b43f0c6c499e3b94271cb2
SeulGi Jeong <sllki1@naver.com>
2018-01-19 02:36:35 +0900

    오류 수정

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,27 +1,27 @@
 package controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import api.MovieApi;
 import dto.MemDTO;
-import service.MemServiceImp;
+import service.MemService;
 
 // http://localhost:8090/finalproject/main
 @Controller
 public class HelloController {
-	private MemServiceImp service;
-	
+	private MemService service;
+
 	public HelloController() {
-		
+
 	}
-	
-	public void setService(MemServiceImp service) {
+
+	public void setService(MemService service) {
 		this.service = service;
 	}
-	
+
 	@RequestMapping("/main")
 	public ModelAndView mainPage() {
 		ModelAndView mav = new ModelAndView();
@@ -30,22 +30,18 @@ public class HelloController {
 		mav.setViewName("index");
 		return mav;
 	}
-	
+
 	@RequestMapping("/signup")
 	public String signUp() {
 		return "signUpForm";
 	}
-	
-<<<<<<< HEAD
-	@RequestMapping("/join")
-=======
+
 	@RequestMapping("/free")
 	public String board() {
 		return "freeboard";
 	}
-	
-	@RequestMapping(value="/join",method=RequestMethod.POST)
->>>>>>> 323ca1d6316bda744762d1df9e6c97dc0fec575f
+
+	@RequestMapping(value = "/join", method = RequestMethod.POST)
 	public ModelAndView join(MemDTO dto) {
 		ModelAndView mav = new ModelAndView();
 		service.registerProcess(dto);
