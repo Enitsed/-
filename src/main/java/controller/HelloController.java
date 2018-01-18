0f5579f931b7b9f8133f8af1853a96cfe4cbff3c
AnJungWoo <sgo316@naver.com>
2018-01-18 19:21:04 +0900

    d

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -2,10 +2,24 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import dto.MemDTO;
+import service.MemServiceImp;
 
 // http://localhost:8090/finalproject/main
 @Controller
 public class HelloController {
+	private MemServiceImp service;
+	
+	public HelloController() {
+		
+	}
+	
+	public void setService(MemServiceImp service) {
+		this.service = service;
+	}
+	
 	@RequestMapping("/main")
 	public String mainPage() {
 		return "index";
@@ -14,5 +28,13 @@ public class HelloController {
 	@RequestMapping("/signup")
 	public String signUp() {
 		return "signUpForm";
+	}
+	
+	@RequestMapping("/join")
+	public ModelAndView join(MemDTO dto) {
+		ModelAndView mav = new ModelAndView();
+		service.register(dto);
+		mav.setViewName("main");
+		return mav;
 	}
 }
