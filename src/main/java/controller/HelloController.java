b6b726e261e53331cd617b72f27c2babb7723141
hchc6480 <rkdgusdn1214@naver.com>
2018-01-18 16:59:56 +0900

    new

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // http://localhost:8090/finalproject/main
+
 @Controller
 public class HelloController {
+	
 	@RequestMapping("/main")
 	public String mainPage() {
 		return "main";
 	}
+	
+	
 }
