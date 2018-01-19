2510f28a00db86165736a6f119ca4349d9df10cf
hchc6480 <rkdgusdn1214@naver.com>
2018-01-19 18:55:28 +0900

    controller

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,8 +1,5 @@
 package controller;
 
-
-
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -16,67 +13,65 @@ import service.MovieService;
 // http://localhost:8090/finalproject/main
 @Controller
 public class HelloController {
-	
+
 	public HelloController() {
-		
+
 	}
+
 	MemService service;
 	MovieService movieservice;
-	
+
 	public void setMovieservice(MovieService movieservice) {
 		this.movieservice = movieservice;
 	}
-	
+
 	public void setService(MemService service) {
 		this.service = service;
 	}
 	/*
-	@RequestMapping("/main")
-	public String mainPage() {
-		return "index";
-	}*/
-	
+	 * @RequestMapping("/main") public String mainPage() { return "index"; }
+	 */
+
 	@RequestMapping("/main")
 	public ModelAndView mainPage() {
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("movie",movieservice.movieInfoProcess());
+		mav.addObject("movie", movieservice.movieInfoProcess());
 		mav.setViewName("index");
 		return mav;
 	}
-	
-	
+
 	@RequestMapping("/signup")
 	public String signUp() {
 		return "signUpForm";
 	}
-	
+
 	@RequestMapping("/login")
 	public String loginPage() {
 		return "loginForm";
 	}
-	
+
 	@RequestMapping("/loginsuccess")
 	public ModelAndView loginsuccess(String kid, HttpSession session) {
 		ModelAndView mav = new ModelAndView();
 		System.out.println(kid);
-		session.setAttribute("kid",kid);
+		session.setAttribute("kid", kid);
 		mav.setViewName("redirect:/main");
 		return mav;
 	}
-	
+
 	@RequestMapping("/logout")
 	public String logout(HttpSession session) {
 		session.removeAttribute("kid");
 		return "redirect:/main";
 	}
-	
+
 	@RequestMapping("/check")
 	public String logincheck(MemDTO dto, HttpSession session) {
-	
+
 		boolean result = service.findProcess(dto);
-		if(result) {
+		if (result) {
 			System.out.println(dto.getMem_id());
-			session.setAttribute("kid",dto.getMem_id());
+			session.setAttribute("kid", dto.getMem_id());
 		}
 		return "redirect:/main";
 	}
