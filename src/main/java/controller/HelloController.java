ae30b10d6331ef50f7de2a95c2f8f522db35d3a7
SeulGi Jeong <sllki1@naver.com>
2018-01-24 16:33:24 +0900

    멤버 컨트롤러 분리

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -21,7 +21,6 @@ import service.MovieService;
 // http://localhost:8090/finalproject/main
 @Controller
 public class HelloController {
-	MemService service;
 	MovieService movieservice;
 
 	public HelloController() {
@@ -30,10 +29,6 @@ public class HelloController {
 
 	public void setMovieservice(MovieService movieservice) {
 		this.movieservice = movieservice;
-	}
-
-	public void setService(MemService service) {
-		this.service = service;
 	}
 
 	@RequestMapping("/main")
@@ -50,70 +45,6 @@ public class HelloController {
 	@RequestMapping("/searchResult")
 	public String searchResult() {
 		return "search_result";
-	}
-
-	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public ModelAndView signUp(MemDTO dto) {
-		ModelAndView mav = new ModelAndView();
-		service.registerProcess(dto);
-		System.out.println("num =" + dto.getMem_num() + " id= " + dto.getMem_id() + " pw=" + dto.getMem_pw() + " name="
-				+ dto.getMem_name() + " email=" + dto.getMem_email() + " address=" + dto.getMem_address() + " sex="
-				+ dto.getMem_sex());
-		mav.setViewName("redirect:/main");
-		return mav;
-	}
-
-	@RequestMapping(value = "/signup", method = RequestMethod.GET)
-	public ModelAndView viewSignUp(MemDTO dto) {
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("signUpForm");
-		return mav;
-	}
-
-	@RequestMapping(value = "/chkId", method = RequestMethod.POST)
-	public @ResponseBody int chkId(String mem_id, HttpServletRequest request) {
-		boolean rs = service.chkIdProcess(mem_id);
-		if (rs)
-			return 1;
-		else
-			return 2;
-	}
-
-	@RequestMapping("/login")
-	public ModelAndView login(MemDTO dto, HttpSession session) {
-		ModelAndView mav = new ModelAndView();
-		boolean rs = service.findProcess(dto);
-		if(rs) {
-			String name = service.login(dto);
-			session.setAttribute("kid", name);
-			mav.setViewName("redirect:/main");
-		}
-		return mav;
-	}
-
-	@RequestMapping("/loginsuccess")
-	public ModelAndView loginsuccess(String kid, HttpSession session) {
-		ModelAndView mav = new ModelAndView();
-		session.setAttribute("kid", kid);
-		mav.setViewName("redirect:/main");
-		return mav;
-	}
-
-	@RequestMapping("/logout")
-	public String logout(HttpSession session) {
-		session.invalidate();
-		return "redirect:/main";
-	}
-
-	@RequestMapping("/check")
-	public String logincheck(MemDTO dto, HttpSession session) {
-
-		boolean result = service.findProcess(dto);
-		if (result) {
-			// System.out.println(dto.getMem_id());
-			session.setAttribute("kid", dto.getMem_id());
-		}
-		return "redirect:/main";
 	}
 
 	@RequestMapping(value = "info", method = RequestMethod.GET)
---
+++ src/main/java/controller/MemberController.java
@@ -0,0 +1,94 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import dto.MemDTO;
+import service.MemService;
+
+@Controller
+public class MemberController {
+	MemService service;
+	
+	public MemberController() {
+		
+	}
+
+	public void setService(MemService service) {
+		this.service = service;
+	}
+	
+
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public ModelAndView signUp(MemDTO dto) {
+		ModelAndView mav = new ModelAndView();
+		service.registerProcess(dto);
+		System.out.println("num =" + dto.getMem_num() + " id= " + dto.getMem_id() + " pw=" + dto.getMem_pw() + " name="
+				+ dto.getMem_name() + " email=" + dto.getMem_email() + " address=" + dto.getMem_address() + " sex="
+				+ dto.getMem_sex());
+		mav.setViewName("redirect:/main");
+		return mav;
+	}
+
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public ModelAndView viewSignUp(MemDTO dto) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("signUpForm");
+		return mav;
+	}
+
+	@RequestMapping(value = "/chkId", method = RequestMethod.POST)
+	public @ResponseBody int chkId(String mem_id, HttpServletRequest request) {
+		boolean rs = service.chkIdProcess(mem_id);
+		if (rs)
+			return 1;
+		else
+			return 2;
+	}
+
+	@RequestMapping("/login")
+	public ModelAndView login(MemDTO dto, HttpSession session) {
+		ModelAndView mav = new ModelAndView();
+		boolean rs = service.findProcess(dto);
+		if(rs) {
+			String name = service.login(dto);
+			session.setAttribute("kid", name);
+			mav.setViewName("redirect:/main");
+		}
+		return mav;
+	}
+
+	@RequestMapping("/loginsuccess")
+	public ModelAndView loginsuccess(String kid, HttpSession session) {
+		ModelAndView mav = new ModelAndView();
+		session.setAttribute("kid", kid);
+		mav.setViewName("redirect:/main");
+		return mav;
+	}
+
+	@RequestMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/main";
+	}
+
+	@RequestMapping("/check")
+	public String logincheck(MemDTO dto, HttpSession session) {
+
+		boolean result = service.findProcess(dto);
+		if (result) {
+			// System.out.println(dto.getMem_id());
+			session.setAttribute("kid", dto.getMem_id());
+		}
+		return "redirect:/main";
+	}
+
+	
+	
+}
